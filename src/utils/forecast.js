ac90507ba93965e379a97e9aaa84cd175d6f2d6a
Linmoy Pradhan <linmoy.reddevil@gmail.com>
2022-03-23 17:26:08 +1100

    added Humidity in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,8 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        ` ${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degree out. It feels like ${body.current.feelslike} degree out.`
+        ` ${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degree out. It feels like ${body.current.feelslike} degree out.
+        Humidity is ${body.current.humidity}.`
       );
     }
   });
